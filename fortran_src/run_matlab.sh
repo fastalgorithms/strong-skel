08af3c71ca9ba87a9000af76084e53f100e7cc22
Manas Rachh <mrachh@flatironinstitute.org>
2021-11-29 20:10:02 -0500

    updating

---
+++ fortran_src/run_matlab.sh
@@ -0,0 +1,4 @@
+IK=2
+NU=80
+NP=5
+/Applications/MATLAB_R2021a.app/bin/matlab -nodesktop -nosplash -nodisplay -r "run test1(${IK},${NU},${NP})"
--- fortran_src/test1.m
+++ fortran_src/test1.m
@@ -1,11 +1,27 @@
-clear
+function test1(ik,npu,norder)
 radii = [1.0;2.0;0.25];
 scales = [1.2;1.0;1.7];
-nu = 20;
-nv = 20;
+nu = npu;
+nv = npu;
 nosc = 5;
-norder = 5;
+norder = norder;
+if(ik == 1)
+    zk = 0.97;
+else
+    zk = 0.97*nu/10;
+end
+fname = ['diary_ik' int2str(ik) '_np' int2str(npu) '_norder' int2str(norder) '_qtime.dat'];
+diary(fname);
 S = wtorus(radii,scales,nosc,nu,nv,norder);
-zpars = complex([1.1; 1j*1.0; 1.0]);
-eps = 1e-2;
+zpars = complex([zk; 1.0; 0.0]);
+eps = 0.51e-7;
 tic, spmat = helm_near_corr(S,zpars,eps); toc;
+zpars = complex([zk; 0.0; 1.0]);
+tic, spmat = helm_near_corr(S,zpars,eps); toc;
+zpars = complex([zk; 1j*zk; 1.0]);
+tic, spmat = helm_near_corr(S,zpars,eps); toc;
+zpars = complex([zk; -1j*zk; 1.0]);
+tic, spmat = helm_near_corr(S,zpars,eps); toc;
+diary('off');
+exit;
+end
