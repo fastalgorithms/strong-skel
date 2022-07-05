040a2b187ad10ed40b92046c5cc17b5fc3f29ff7
Manas Rachh <mrachh@flatironinstitute.org>
2022-07-05 09:49:29 -0400

    removing global variables

--- test/ie_fmm3dbie_ss_paper_final_ex.m
+++ test/ie_fmm3dbie_ss_paper_final_ex.m
@@ -16,7 +16,7 @@ function [varargout] =  ie_fmm3dbie_ss_paper_final_ex(igeomtype,iref,npu,norder,
 
 if(nargin == 0)
     igeomtype = 1;
-    npu = 5;
+    npu = 10;
     iref = 0;
     norder = 3;
     zk = 1.0;
@@ -27,6 +27,8 @@ addpath('../fortran_src')
 addpath('../')
 addpath('../sv')
 addpath('../mv')
+addpath('../src')
+addpath('../src/helm_dirichlet');
 run('../../FLAM/startup.m');
 occ = 4096;
 p = 512;
@@ -105,7 +107,10 @@ fprintf('quad: %10.4e (s) / %6.2f (MB)\n',tquad,w.bytes/1e6)
 
 % Factor the matrix using skeletonization (verbose mode)
 opts = struct('verb',1,'symm','n','zk',zk);
-tic, F = srskelf_asym_new(@Afun,x,occ,rank_or_tol,@pxyfun,opts); tfac = toc;
+
+Afun_use = @(i,j) Afun_helm_dirichlet(i,j,x,zpars,nu,area,P,S);
+pxyfun_use = @(x,slf,nbr,proxy,l,ctr) pxyfun_helm_dirichlet(x,slf,nbr,proxy,l,ctr,zpars,nu,area);
+tic, F = srskelf_asym_new(Afun_use,x,occ,rank_or_tol,pxyfun_use,opts); tfac = toc;
 w = whos('F');
 fprintf([repmat('-',1,80) '\n'])
 fprintf('mem: %6.4f (GB)\n',w.bytes/1048576/1024)
@@ -114,7 +119,7 @@ fprintf('mem: %6.4f (GB)\n',w.bytes/1048576/1024)
 % 
 q = rand(m,1)-0.5; + 1j*(rand(m,1)-0.5);
 nu2 = zeros(3,m);
-B = Kfun(x,xyz_in,zstmp,nu2)*q;
+B = helm_dirichlet_kernel(x,xyz_in,zstmp,nu2)*q;
 B = B.*sqrt(area).';
 
 % Solve for surface density
@@ -123,7 +128,7 @@ X = X./sqrt(area).';
 Y = lpcomp_helm_comb_dir(sinfo,zpars,X,xyz_out,rank_or_tol);
 
 % Compare against exact field
-Z = Kfun(xyz_out,xyz_in,zstmp,nu2)*q;
+Z = helm_dirichlet_kernel(xyz_out,xyz_in,zstmp,nu2)*q;
 tmp1 = sqrt(area)'.*X;
 ra = norm(tmp1);
 e = norm(Z - Y)/ra;
@@ -143,16 +148,19 @@ fprintf('pde: %10.4e\n',e)
 
 
 diary('off')
+return
 
 [uinc,xd,xn,thet] = get_uinc(ndir,sinfo,zk);
 tic, Xincsol = srskelf_sv_nn(F,uinc); tsolve = toc;
 
 exd = exp(-1j*zk*xd);
 dfar = -1j*zpars(3)*zk*xn.*exd;
-sfar = zpars(2).*exd;
+sfar = zpars(2)*exd;
 ww = sinfo.wts;
 wwr = repmat(ww,[1,ndir]);
 ufar = (dfar+sfar).*Xincsol.*sqrt(wwr)/4/pi;
+
+
 ufar = sum(ufar,1);
 varargout{1} = thet;
 varargout{2} = ufar;
@@ -166,75 +174,5 @@ save(fsol,'ufar','thet','Xincsol');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function K = Kfun(x,y,zpuse,nuuse)
-% KFUN(X,Y,zpars,NU) computes the Helmholtz potential evaluated
-% pairwise between points in X and points in Y (does not handle the
-% singularity).  zpars says which layer potential to use
-% using the surface normal vectors in NU.
-dx = bsxfun(@minus,x(1,:)',y(1,:));
-dy = bsxfun(@minus,x(2,:)',y(2,:));
-dz = bsxfun(@minus,x(3,:)',y(3,:));
-dr = sqrt(dx.^2 + dy.^2 + dz.^2);
-zexp = exp(1j*zpuse(1)*dr);
-slp = 1/(4*pi)./dr.*zexp;
-rdotn = bsxfun(@times,dx,nuuse(1,:)) + bsxfun(@times,dy,nuuse(2,:)) + ...
-          bsxfun(@times,dz,nuuse(3,:));
-dlp = 1/(4*pi).*rdotn./dr.^3.*zexp.*(1.0-1j*zpuse(1)*dr);
-K = zpuse(2)*slp + zpuse(3)*dlp;
-K(dr == 0) = 0;
-end
-
-function A = Afun(i,j)
-% AFUN(I,J) computes entries of the matrix A to be factorized at the
-% index sets I and J.  This handles the near-field correction.
-if isempty(i) || isempty(j)
-  A = zeros(length(i),length(j));
-  return
-end
-[I,J] = ndgrid(i,j);
-A = bsxfun(@times,Kfun(x(:,i),x(:,j),zpars,nu(:,j)),area(j));
-M = spget(i,j);
-idx = abs(M) ~= 0;
-A(idx) = M(idx);
-A(I == J) = A(I == J) + 0.5*zpars(3);
-
-A = bsxfun(@times,sqrt(area(i)).',A);
-A = bsxfun(@times,A,1.0./sqrt(area(j)));
-end
-
-% proxy function
-function [Kpxy,nbr] = pxyfun(x,slf,nbr,proxy,l,ctr)
-% PXYFUN(X,SLF,NBR,L,CTR) computes interactions between the points
-% X(:,SLF) and the set of proxy points by scaling the proxy sphere to 
-% appropriately contain a box at level L centered at CTR and then
-% calling KFUN
-pxy = bsxfun(@plus,proxy*l,ctr');
-Kpxy1 = Kfun(pxy,x(:,slf),zstmp,nu(:,slf));
-Kpxy1 = bsxfun(@times,Kpxy1,sqrt(area(slf)));
-Kpxy3 = bsxfun(@times,Kfun(pxy,x(:,slf),zpars,nu(:,slf)),sqrt(area(slf)));
-Kpxy = [Kpxy1;Kpxy3];
-dx = x(1,nbr) - ctr(1);
-dy = x(2,nbr) - ctr(2);
-dz = x(3,nbr) - ctr(3);
-dist = sqrt(dx.^2 + dy.^2 + dz.^2);
-nbr = nbr(dist/l < 1.5);
-end
-
-function A = spget(I_,J_)
-% SPGET(I_,J_) computes entries of a sparse matrix of near-field
-% corrections that should be added to the kernel matrix, as used in
-% AFUN.
-m_ = length(I_);
-n_ = length(J_);
-[I_sort,E] = sort(I_);
-P(I_sort) = E;
-A = zeros(m_,n_);
-[I_,J_,S_] = find(S(:,J_));
-idx = ismembc(I_,I_sort);
-I_ = I_(idx);
-J_ = J_(idx);
-S_ = S_(idx);
-A(P(I_) + (J_ - 1)*m_) = S_;
-end
 end
 
