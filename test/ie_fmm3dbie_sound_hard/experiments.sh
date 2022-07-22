e4cf5563150c012967d52aa2d59f0ab8f8eb8981
Srinath Kailasa <srinathkailasa@gmail.com>
2022-07-22 11:13:39 -0400

    Fix loop

--- test/ie_fmm3dbie_sound_hard/experiments.sh
+++ test/ie_fmm3dbie_sound_hard/experiments.sh
@@ -6,22 +6,23 @@ RTOL=0.00005
 M=50
 
 # Experiment matrix
-NPUS=( 10 )
-NORDERS=( 4 )
-OCCS=( 500 )
+NPUS=(10 20 30 40)
+NORDERS=(4)
+OCCS=(500)
 
 # Construct and submit a job from experiment matrix
-for NORDER in $NORDERS
+for NORDER in ${NORDERS[@]}
 do
-    for NPU in $NPUS
+    for NPU in ${NPUS[@]}
     do
-        for OCC in $OCCS
+        for OCC in ${OCCS[@]}
         do
         scommand="sbatch -J NORDER=$NORDER.NPU=${NPU}.OCC=${OCC}.run "
         scommand+="--export=NORDER=${NORDER},NPU=${NPU},OCC=${OCC},ZK=${ZK},RTOL=${RTOL},M=${M} "
         scommand+="--output=NORDER_${NORDER}_NPU_${NPU}_OCC_${OCC}.out "
         scommand+="ie_fmm3dbie_sound_hard.slurm"
         echo "submitting: $scommand"
+        echo ""
         $scommand
         done
     done
